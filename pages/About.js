a21e6301a44e7bc056f4634630bc4f2478e7c480
ayomidetobi <ayomidestobsy10@gmail.com>
2023-02-15 17:55:36 +0100

    splice

--- pages/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { MDBInput } from '../mdb-react-ui-kit';
-const About = () => {
-    return (
-        <MDBInput label='Email input' id='typeEmail' type='email' />
-    );
-}
-
-export default About;
